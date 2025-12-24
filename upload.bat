34d981cd7a9b794611cf38df912ff47b1ca17be2
Chera-Kang <chera.workspace@gmail.com>
2025-12-24 17:17:30 +0900

    update (2025-12-24 17:17)

--- upload.bat
+++ upload.bat
@@ -9,9 +9,10 @@ echo ========================================
 :: 1. 변경사항 등록
 git add .
 
-:: 2. 커밋 (사용자님이 쓰시던 "update" 메시지 유지)
-:: 현재 날짜와 시간을 뒤에 붙여서 언제 업데이트했는지 구분하기 쉽게 했습니다.
-set datetime=%date% %time%
+:: 2. 커밋
+set currenttime=%time:~0,5%
+set datetime=%date% %currenttime%
+
 git commit -m "update (%datetime%)"
 
 :: 3. 서버로 업로드
